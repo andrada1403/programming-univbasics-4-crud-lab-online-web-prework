6faea699c5c8039b3b54c9ef1f7ff041169d6ed3
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-09-24 16:32:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  create_an_array=["skippy","andrada","pp5","popsicle"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  element="arrays"
+  array.push(#{element})
 end
 
 def add_element_to_start_of_array(array, element)
